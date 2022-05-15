6c4f7eb016b1a02e431f1722e25597a75e4df49d
Giolti2 <105230322+Giolti2@users.noreply.github.com>
2022-05-15 19:45:27 +0200

    Add files via upload

---
+++ TESTI/s1b1.js
@@ -0,0 +1,11 @@
+var s1b1 = `Il soggetto #80085 (DICK ANDREW) sembra particolarmente stimato dai prolet, probabilmente a causa della sua mansione. Durante le due visite al bar assegnatogli ha svolto diligentemente il suo lavoro: ha consegnato le informazioni al barista e ha registrato l’indice di gradimento dei prodotti audiovisivi.
+Non si è limitato però solo a questo: parla con i prolet più di quanto il suo lavoro richieda, registrando con cura le preferenze di questi. Presumo quindi che le attenzioni che i prolet gli forniscono siano dovute a questa sua attitudine.
+
+Per la completezza del rapporto allego alcuni appunti sulle sue conversazioni:
+
+
+soggetto: ”mi dica i film che preferisce”
+
+proletA: “sono sicuro che solo i film di guerra possano accompagnare una birra.”
+
+proletB: “sono tutte fesserie, nulla è come i film di una volta”`
--- miniamor2.js
+++ miniamor2.js
@@ -90,13 +90,65 @@ function updateSliders(){
 
 updateSliders();
 
+//bottoni guardia
+
+var guardButtons = [
+  document.getElementById("button1"),
+  document.getElementById("button2"),
+  document.getElementById("button3")
+];
+
+for (var i = 0; i < guardButtons.length; i++){
+  guardButtons[i].addEventListener("click", activateGuard);
+}
+
+var guarded = 0;
+
+function activateGuard(args){
+  var index = args.target.id[args.target.id.length-1] - 1;
+  guarded = index;
+
+  for (var i = 0; i < guardButtons.length; i++){
+    guardButtons[i].classList.remove("active");
+  }
+
+  guardButtons[index].classList.add("active");
+
+}
+
+//invio
+
+var week = 0;
+
+var sendButton = document.getElementById("send");
+
+function send(){
+  if(week == 0){
+    for(var i = 0; i < contents.length; i++){
+      contents[i].getElementsByClassName("reports")[0].innerHTML = "";
+    }
+  }
+  newInfo(0);
+  newInfo(1);
+  //newInfo(2);
+  //news();
+  week += 1;
+}
+
+sendButton.addEventListener("click", send);
+
+//generazione testi
+
 function newInfo(subject){
   var container = document.getElementById("content"+subject);
   container.weeks = container.getElementsByClassName("weeks")[0];
   container.reports = container.getElementsByClassName("reports")[0];
 
   container.reports.append(newReport(subject));
-  //container.weeks.append(newWeek(subject));
+  container.weeks.append(newWeek(subject));
+  if(guarded == subject){
+    container.reports.append(newGuardReport(subject));
+  }
 }
 
 function newWeek(subject){
@@ -106,21 +158,21 @@ function newWeek(subject){
   var anchor = document.createElement("a");
   anchor.href = "#" + subject + "w0";
 
-  anchor.innerHTML = "Settimana " + 1;
+  anchor.innerHTML = "Settimana " + parseInt(week + 1);
   date.append(anchor);
 
   return date;
 }
 
 function newReport(subject){
-  var text = s1w1a;
+  var text = chooseText(subject);
 
   var report = document.createElement("div");
   report.classList.add("report");
 
   var anchor = document.createElement("a");
   anchor.name = subject + "w0";
-  anchor.innerHTML = "<b>SETTIMANA " + 1 + "</b><br><br>"
+  anchor.innerHTML = "<b>SETTIMANA " + parseInt(week + 1) + "</b><br><br>"
 
   report.append(anchor);
   report.innerHTML += text;
@@ -128,4 +180,94 @@ function newReport(subject){
   return report;
 }
 
-newInfo(0);
+function newGuardReport(subject){
+  var text = chooseGuardText(subject);
+
+  var report = document.createElement("div");
+  report.classList.add("report");
+
+  report.innerHTML += text;
+
+  return report;
+}
+
+function chooseText(subject){
+  switch(subject){
+    case 0:
+      return andrewDick()
+    case 1:
+      return peterMinson()
+    case 2:
+      return emmaOrnstein()
+  }
+}
+
+function chooseGuardText(subject){
+  var file = "";
+  switch(subject){
+    case 0:
+      file += "s1b"+parseInt(week+1);
+      break;
+    case 1:
+      break;
+    case 2:
+      break;
+  }
+
+  console.log(file);
+  return window[file];
+}
+
+function andrewDick(){
+  var file = "s1";
+  switch(week){
+    case 0:
+      file += "w1";
+      if(values[0]<=25){
+        file += "a1";
+      }
+      else if(values[0]<=45){
+        file += "a2";
+      }
+      else{
+        file += "a3";
+      }
+      break;
+    case 1:
+      break;
+    case 2:
+      break;
+    case 3:
+      break;
+  };
+
+  console.log(file);
+  return window[file];
+}
+
+function peterMinson(){
+  var file = "s2";
+  switch(week){
+    case 0:
+      file += "w1";
+      if(values[0]<=25){
+        file += "a1";
+      }
+      else if(values[0]<=45){
+        file += "a2";
+      }
+      else{
+        file += "a3";
+      }
+      break;
+    case 1:
+      break;
+    case 2:
+      break;
+    case 3:
+      break;
+  };
+
+  console.log(file);
+  return window[file];
+}
