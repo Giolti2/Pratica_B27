0babc2cfd100a4527155a574f6c0b2ca2f097638
Giolti2 <105230322+Giolti2@users.noreply.github.com>
2022-05-18 23:15:13 +0200

    Add files via upload

---
+++ TESTI/e6.js
@@ -0,0 +1,10 @@
+var e6 = `
+<b>COMUNICAZIONE DE IL MINISTERO DELL’AMORE:</b><br>
+
+Ottimo lavoro compagno, il suo intervento ha sventato le attività di due pericolosi psicocriminali.<br><br>
+Come da regolamento, la sua segnalazione ha portato ad una preliminare verifica della malafede nelle azioni dei due soggetti: sono state effettuate altre indagini riguardo le loro attività psicocriminali, creando un quadro generale più chiaro di quanto stesse accadendo.<br><br>
+È infatti emerso che il soggetto #87013 (MINSON PETER) sottraesse informazioni al Ministero della Verità con uno stratagemma e che, tramite una scusa o l’altra, le facesse avere al soggetto #80085 (DICK ANDREW). <br><br>
+Quest’ultimo si occupava invece di scovare prolet propensi all’apprendimento e di istruirli tramite il materiale trafugato dal Ministero. A quanto pare l’obbiettivo ultimo era l’emancipazione dei prolet, nella speranza di una possibile rivolta futura, ma, grazie al suo diligente lavoro, questa pericolosa minaccia è stata sventata. I due soggetti sono stati vaporizzati e ora non potranno più minacciare l’integrità della nostra grande nazione.<br><br>
+
+Gloria al Grande Fratello
+`
