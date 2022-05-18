344f86155244b9e1d4b465c30abb441e4e606aeb
Giolti2 <105230322+Giolti2@users.noreply.github.com>
2022-05-18 23:15:31 +0200

    Add files via upload

---
+++ TESTI/e4.js
@@ -0,0 +1,10 @@
+var e4 = `
+<b>COMUNICAZIONE DE IL MINISTERO DELL’AMORE:</b><br>
+
+Ottimo lavoro compagno, il suo intervento ha sventato le attività di uno pericoloso psicocriminale.<br><br>
+Come da regolamento, la sua segnalazione ha portato ad una preliminare verifica della malafede nelle azioni del soggetto. I suoi sospetti sono infatti stati confermati, ma invito una sua maggiore attenzione, in quanto, tra i condomini che le sono stati affidati era presente un secondo psicocriminale, fortunatamente segnalatoci dal soggetto #73921 (ORNSTEIN EMMA).<br><br> 
+È infatti emerso che il soggetto #87013 (MINSON PETER) sottraesse informazioni al Ministero della Verità con uno stratagemma e che, tramite una scusa o l’altra, le facesse avere al soggetto #80085 (DICK ANDREW). <br><br>
+Quest’ultimo si occupava invece di scovare prolet propensi all’apprendimento e di istruirli tramite il materiale trafugato dal Ministero. A quanto pare l’obbiettivo ultimo era l’emancipazione dei prolet, nella speranza di una possibile rivolta futura, ma, grazie al suo diligente lavoro, questa pericolosa minaccia è stata sventata. I due soggetti sono stati vaporizzati e ora non potranno più minacciare l’integrità della nostra grande nazione.<br><br>
+
+Gloria al Grande Fratello
+`
---
+++ TESTI/e5.js
@@ -0,0 +1,9 @@
+var e5 = `
+<b>COMUNICAZIONE DE IL MINISTERO DELL’AMORE:</b><br>
+
+Ottimo lavoro compagno, il suo intervento ha sventato le attività di un pericoloso psicocriminale e ha portato all’internamento di un soggetto mentalmente instabile.<br><br>
+Come da regolamento, la sua segnalazione ha portato ad una preliminare verifica della malafede nelle azioni del soggetto #80085 (DICK ANDREW). I suoi sospetti sono risultati fondati: il soggetto si occupava di scovare prolet propensi all’apprendimento e quindi di istruirli tramite materiale ancora ignoto. A quanto pare l’obiettivo ultimo era l’emancipazione dei prolet, nella speranza di una possibile rivolta futura, ma grazie al suo diligente lavoro questa pericolosa minaccia è stata sventata. <br><br>Supponiamo però che il soggetto #80085 (DICK ANDREW) avesse un complice, ancora a piede libero, che lo aiutasse tramite la raccolta di documenti e informazioni da destinare ai prolet. Invitiamo quindi ad una maggiore attenzione nel controllo dei soggetti e alla segnalazione di ogni stranezza e incongruenza. <br><br>
+Il soggetto #73921 (ORNSTEIN EMMA), invece, all’intervento della pattuglia della psicopolizia, ha cominciato a delirare e ad accusare ogni inquilino del condominio di psicoreato. Per questo è stata avviata la procedura di internamento e verrà svolta un’indagine per verificare che non ci siano altri psicocriminali nel palazzo che possano minacciare l’integrità della nostra grande nazione.<br><br>
+
+Gloria al Grande Fratello
+`
